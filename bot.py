6f4d1ded2b10e391bb2f5d07c031603de43faa0b
Tate <dracoranger@hotmail.com>
2018-05-30 22:33:54 -0400

    Minimal working version?

--- bot.py
+++ bot.py
@@ -1,10 +1,11 @@
-#keyBot
+#CoHBot
 #By DracoRanger
 import asyncio
 import random
+from collections import deque
 import discord
 from discord.ext import commands
-#import logging
+
 
 client = discord.Client()
 bot = commands.Bot(command_prefix="!", description="")
@@ -26,10 +27,10 @@ currentDealer = 0
 
 black = ''
 white = ''
-with open cardsBlack as b:#I NEED A QUEUE!
-    black = b.readlines().randomize()
-with open cardsWhite as w:
-    white = w.readlines().randomize()
+with open(cardsBlack) as b:#I NEED A QUEUE!
+    black = deque(random.shuffle(b.readlines()))
+with open(cardsWhite) as w:
+    white = deque(random.shuffle(w.readlines()))
 
 
 
@@ -51,6 +52,7 @@ async def on_message(message):
     global white
     global cardStorage
     global currentDealer
+    global playersGotten
     if message.author == client.user:
         return
     if message.channel == client.get_channel(channelNum):
@@ -59,44 +61,52 @@ async def on_message(message):
         '''
         if message.content.startswith('!startGame') and not gameStarted:
             temp = "Ok, game is starting.\nAnyone who wants to play reply with !in.\nSomeone responsible type !done."
-            gameStarted = not gameStarted
+            gameStarted = True
+            playersGotten = False
             await client.send_message(message.channel, temp)
         elif message.content.startswith('!in') and gameStarted:
             if message.author not in players:
                 players.append(message.author)
         elif message.content.startswith('!done') and gameStarted:
             playersGotten = True
-            init = 0
             for i in players:
                 hand = []
-                for j in range(init, init + 5):
+                for j in range(0, 7):
+                    card = black.pop()
                     hand.append(black[j])
+                    black.append(card)
                 cardStorage.append(hand)
-            for i in range(0, len(player)):
-                await client.send_message(player[i],cardStorage[i])
+            for i in range(0, len(players)):
+                await client.send_message(players[i], cardStorage[i])
         elif message.content.startswith('!deal') and playersGotten:
-            for i in range(0, len(player)):
+            for i in range(0, len(players)):
                 if i == currentDealer:
-                    await client.send_message(player[i],"Choose the best!")
-                    await client.send_message(message.channel, white.next())
+                    await client.send_message(players[i], "Choose the best!")
+                    wCard = white.pop()
+                    white.append(wCard)
+                    await client.send_message(message.channel, wCard)
             currentDealer = currentDealer + 1
+            if currentDealer == len(players):
+                currentDealer = 0
         elif message.content.startswith('!choose') and playersGotten:
-            if int(message.content[7:-1])>=0 and int(message.content[7:-1])<=5:
-                message.author location in players
-                await client.send_message(message.channel, cardStorage[i][int(message.content[7:-1])])
-                cardStorage[i][int(message.content[7:-1])] remove
-                cardStorage[i].append(black.next())
+            if int(message.content[8:-1]) >= 0 and int(message.content[8:-1]) <= 5:
+                locl = players.index(message.author)
+                await client.send_message(message.channel, cardStorage[locl][int(message.content[8:-1])])
+                del cardStorage[locl][int(message.content[8:-1])]
+                card = black.pop()
+                cardStorage[locl].append(card)
+                black.append(card)
 
 
         '''
         prints all commands
         '''
         if message.content.startswith('!help'):
-            keylistDaily = "**!keysDaily** = prints a list of the daily games, works in either server or in pms"
-            keylistWeekly = "**!keysWeekly** = prints a list of the weekly games, works in either server or in pms"
-            gib = "**!gib [gameName] [key]**= gives a key to the bot, only works in pms"
-            takeDaily = "**!takeDaily [gameName]** = messages you with the game's key, works only in server, recieve key in pm, message posted to server"
-            takeWeekly = "**!takeWeekly [gameName]** = messages you with the game's key, works only in server, recieve key in pm, message posted to server"
+            keylistDaily = "**!startGame** = "
+            keylistWeekly = "**!in** = "
+            gib = "**!done** = "
+            takeDaily = "**!deal** = "
+            takeWeekly = "**!choose** = "
 
             ret = keylistDaily+'\n'+takeDaily+'\n\n'+keylistWeekly+'\n'+takeWeekly+'\n\n'+gib+'\n'
             await client.send_message(message.channel, ret)
