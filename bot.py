c647a8e3a41c858ad7d5d71def6f8e0cdea4ef63
Tate <dracoranger@hotmail.com>
2018-05-31 10:20:35 -0400

    More updates

--- bot.py
+++ bot.py
@@ -27,10 +27,14 @@ currentDealer = 0
 
 black = ''
 white = ''
-with open(cardBlack) as b:#I NEED A QUEUE!
-    black = deque(random.shuffle(b.readlines()))
+with open(cardsBlack) as b:#I NEED A QUEUE!
+    temp = b.readlines()
+    random.shuffle(temp)
+    black = deque(temp)
 with open(cardsWhite) as w:
-    white = deque(random.shuffle(w.readlines()))
+    temp = w.readlines()
+    random.shuffle(temp)
+    white = deque(temp)
 
 
 
@@ -60,10 +64,10 @@ async def on_message(message):
         plays game
         '''
         if message.content.startswith('!startGame') and not gameStarted:
-            temp = "Ok, game is starting.\nAnyone who wants to play reply with !in.\nSomeone responsible type !done."
+            stor = "Ok, game is starting.\nAnyone who wants to play reply with !in.\nSomeone responsible type !done."
             gameStarted = True
             playersGotten = False
-            await client.send_message(message.channel, temp)
+            await client.send_message(message.channel, stor)
         elif message.content.startswith('!in') and gameStarted:
             if message.author not in players:
                 players.append(message.author)
@@ -77,38 +81,59 @@ async def on_message(message):
                     black.append(card)
                 cardStorage.append(hand)
             for i in range(0, len(players)):
-                await client.send_message(players[i], cardStorage[i])
+                car = ''
+                num = 0
+                for j in cardStorage[i]:
+                    car = car + str(num)+': ' + j
+                    num = num + 1
+                await client.send_message(players[i], car)
         elif message.content.startswith('!deal') and playersGotten:
             for i in range(0, len(players)):
                 if i == currentDealer:
-                    await client.send_message(players[i], "Choose the best!")
+                    await client.send_message(players[i], "Choose the winner")
                     wCard = white.pop()
-                    white.append(wCard)
                     await client.send_message(message.channel, wCard)
+                    white.append(wCard)
+                else:
+                    await client.send_message(players[i], "Select your card")
+        elif message.content.startswith('!selectWinner') and players[currentDealer] == message.author:
+            await client.send_message(players[int(message.content[14])], "Good choice!\nYou won the round!")
+            for i in range(0, len(players)):
+                car = ''
+                num = 0
+                for j in cardStorage[i]:
+                    car = car + str(num)+': ' + j
+                    num = num + 1
+                await client.send_message(players[i], car)
             currentDealer = currentDealer + 1
             if currentDealer == len(players):
                 currentDealer = 0
-        elif message.content.startswith('!choose') and playersGotten:
-            if int(message.content[8:-1]) >= 0 and int(message.content[8:-1]) <= 5:
-                locl = players.index(message.author)
-                await client.send_message(message.channel, cardStorage[locl][int(message.content[8:-1])])
-                del cardStorage[locl][int(message.content[8:-1])]
-                card = black.pop()
-                cardStorage[locl].append(card)
-                black.append(card)
-
-
-        '''
-        prints all commands
-        '''
-        if message.content.startswith('!help'):
-            keylistDaily = "**!startGame** = "
-            keylistWeekly = "**!in** = "
-            gib = "**!done** = "
-            takeDaily = "**!deal** = "
-            takeWeekly = "**!choose** = "
-
-            ret = keylistDaily+'\n'+takeDaily+'\n\n'+keylistWeekly+'\n'+takeWeekly+'\n\n'+gib+'\n'
+        elif message.content.startswith('!clear'):
+            gameStarted = False
+            playersGotten = False
+            players = []
+            cardStorage = []
+            currentDealer = 0
+            await client.send_message(message.channel, "Cleared")
+        elif message.content.startswith('!help'):
+            start_game = "**!startGame** = Begins the game"
+            join_in = "**!in** = join game"
+            finished = "**!done** = everyone who wants has joined"
+            deal = "**!deal** = distribute a black card"
+            select = "**!choose** + int = player selects a card from their hand, posts it to the main chat, so do it in a private channel"
+            winner = "**!selectWinner** + int = Chooser selects a winner"
+            clear = "**!clear** = ends the game and gets it ready for the next game"
+            ret = start_game+'\n'+join_in+'\n\n'+finished+'\n'+deal+'\n\n'+select+'\n'+winner+'\n\n'+clear
             await client.send_message(message.channel, ret)
+
+    if message.content.startswith('!choose') and playersGotten and message.channel.is_private:
+        #implement error checking
+        if int(message.content[8]) >= 0 and int(message.content[8]) <= 6:
+            locl = players.index(message.author)
+            await client.send_message(client.get_channel(channelNum), str(locl)+': '+cardStorage[locl][int(message.content[8])])
+            del cardStorage[locl][int(message.content[8])]
+            card = black.pop()
+            cardStorage[locl].append(card)
+            black.append(card)
 
 client.run(token)
